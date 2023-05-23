6667111a8f355a644f2c82d2265194d32c3e13f4
HaThiLinh2204 <111418592+HaThiLinh2204@users.noreply.github.com>
2023-05-23 17:19:58 +0700

    part2

--- shop/src/components/Header/header.js
+++ shop/src/components/Header/header.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import {BsFillTelephonePlusFill, BsFillCartPlusFill} from "react-icons/bs";
-import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import { BsFillTelephonePlusFill, BsFillCartPlusFill } from "react-icons/bs";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
 
-import './Header.css'
+import "./Header.css";
 
 function Header() {
   const [activeTab, setActiveTab] = useState("home");
@@ -17,36 +17,42 @@ function Header() {
     <div className="header-page">
       <div className="topbar">
         <div className="leftbar">
-            <div className="header-icon"> <BsFillTelephonePlusFill/></div>
-            <p>Địa chỉ 102 Thái Thịnh, Đống Đam Hà Nội - Hottline: 1900.2812</p>
+          <div className="header-icon">
+            {" "}
+            <BsFillTelephonePlusFill />
+          </div>
+          <p>Địa chỉ 102 Thái Thịnh, Đống Đam Hà Nội - Hottline: 1900.2812</p>
         </div>
         <div className="rightbar">
-            <i className="">
-                <PermIdentityIcon/>
-                <p>Tài khoản</p>
-            </i>
-            <i className="">
-                <BsFillCartPlusFill/>
-                <p>Giỏ hàng </p>
-            </i>
+          <i className="">
+            <PermIdentityIcon />
+            <p>Tài khoản</p>
+          </i>
+          <i className="">
+            <BsFillCartPlusFill />
+            <p>Giỏ hàng </p>
+          </i>
         </div>
       </div>
       <div className="downbar">
-
         <header className="header">
-          <h1 className="title-page">Fashion Shop</h1>
+          <h1 className="title-page">FURLA</h1>
         </header>
         <div className="tabbar header-tab">
-            <Link to='/'>
+          <Link to="/">
             <button
-            className={activeTab === "home" ? "active" : ""}
-            onClick={() => handleTabClick("home")}
+              className={activeTab === "home" ? "active" : ""}
+              onClick={() => handleTabClick("home")}
+            >
+              Trang chủ
+            </button>
+          </Link>
+          <button
+            className={activeTab === "giay" ? "active" : ""}
+            onClick={() => handleTabClick("about")}
           >
-            Trang chủ
+            Giày
           </button>
-
-            </Link>
-
           <button
             className={activeTab === "about" ? "active" : ""}
             onClick={() => handleTabClick("about")}
@@ -55,8 +61,8 @@ function Header() {
           </button>
           <Link to="/giay">
             <button
-              className={activeTab === "giay" ? "active" : ""}
-              onClick={() => handleTabClick("giay")}
+              className={activeTab === "phukien" ? "active" : ""}
+              onClick={() => handleTabClick("phukien")}
             >
               Phụ kiện
             </button>
@@ -68,11 +74,10 @@ function Header() {
             Thời trang
           </button>
         </div>
-
       </div>
       {/* {activeTab === "home" && <p>This is the home page content.</p>} */}
-        {activeTab === "about" && <p>This is the about page content.</p>}
-        {activeTab === "contact" && <p>This is the contact page content.</p>}
+      {activeTab === "about" && <p>This is the about page content.</p>}
+      {activeTab === "contact" && <p>This is the contact page content.</p>}
     </div>
   );
 }
